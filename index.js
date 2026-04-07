1d67626c4995d17f6cf3ec4823cab7eaea18b69c
Aadiya Chauhan <caaditya2003@gmail.com>
2026-04-07 12:10:18 +0530

    Initial Commit

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require("express")
+const urlRoute = require("./routes/url")
+const { connect_to_db } = require("./connect")
 
 const app = express();
 const PORT = 5050;
+
+connect_to_db("mongodb://localhost:27017/url-shortner").then(() => {
+    console.log("Database connected")
+})
+
+app.use('/url', urlRoute);
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`)
